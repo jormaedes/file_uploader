d0c58a9224edf52e832f75efa536e133d8f32806
Jormaedes Luís <jormahed@gmail.com>
2026-09-02 11:41:10 +0100

    feat: implement dynamic folder creation with Cloudinary integration and update navigation routing

--- routers/homeUserRouter.js
+++ routers/homeUserRouter.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { isAuthenticated, isGuest } from '../middleware/auth.js';
 import { prisma } from '../lib/prisma.js';
+import cloudinary from '../lib/cloudinary.js';
 
 const homeUserRouter = Router();
 
@@ -80,6 +81,60 @@ homeUserRouter.get('/:username/folders/:folderId', isAuthenticated, async (req, 
 			folderChildren: folderChildren,
 			files: files,
 		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send('Internal server error');
+	}
+});
+
+async function getPathToFolder(folderId, userName) {
+	let pathToFolder = '';
+	if (folderId) {
+		const currentFolder = await prisma.folder.findFirst({
+			where: {
+				id: parseInt(folderId),
+			},
+		});
+		if (!currentFolder) {
+			return null;
+		}
+		pathToFolder = await getPathToFolder(currentFolder.parentId, userName) + `${currentFolder.name}/`;
+	}
+	return pathToFolder;
+}
+
+homeUserRouter.post('/:username/folders/:folderId/createFolder', isAuthenticated, async (req, res) => {
+	try {
+		const { folderName } = req.body;
+		const { username, folderId } = req.params;
+		const userId = req.session.userId;
+		const userAuth = await prisma.user.findUnique({ where: { id: userId } });
+		if (username !== userAuth.username) {
+			return res.status(403).send('Forbidden');
+		}
+		const pathToFolder = await getPathToFolder(folderId, username);
+		console.log(pathToFolder);
+		if (!pathToFolder) {
+			return res.status(404).send('Folder not found');
+		}
+		await cloudinary.api.create_folder(`${pathToFolder}${folderName}`);
+		const currentFolder = await prisma.folder.findFirst({
+			where: {
+				id: parseInt(folderId),
+			},
+		});
+		if (!currentFolder) {
+			return res.status(404).send('Folder not found');
+		}
+		const newFolder = await prisma.folder.create({
+			data: {
+				name: folderName,
+				userId: userAuth.id,
+				parentId: currentFolder.id,
+			},
+		});
+		res.redirect(`/home/${username}/folders/${currentFolder.id}`);
+		// res.send('Success');
 	} catch (error) {
 		console.log(error);
 		res.status(500).send('Internal server error');
