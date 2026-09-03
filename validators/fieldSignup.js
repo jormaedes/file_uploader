d64dabdd91811d37a52caddf107d82190d7a5949
Jormaedes Luís <jormahed@gmail.com>
2026-09-03 10:16:27 +0100

    fix: update name validation to require non-empty strings and minimum 2 characters

--- validators/fieldSignup.js
+++ validators/fieldSignup.js
@@ -1,8 +1,8 @@
 import { body } from "express-validator";
 
 export const fieldSignup = [
-	body('firstName').trim().isAlpha().withMessage('First name must be alphabetic').isLength({ min: 1 }).withMessage('First name is required'),
-	body('lastName').trim().isAlpha().withMessage('Last name must be alphabetic').isLength({ min: 1 }).withMessage('Last name is required'),
+	body('firstName').trim().notEmpty().withMessage('First name is required').isLength({ min: 2 }).withMessage('First name must be at least 2 characters long'),
+	body('lastName').trim().notEmpty().withMessage('Last name is required').isLength({ min: 2 }).withMessage('Last name must be at least 2 characters long'),
 	body('username').trim().isAlphanumeric().withMessage('Username must be alphanumeric').isLength({ min: 1 }).withMessage('Username is required'),
 	body('password').isLength({ min: 1 }).withMessage('Password is required').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 	body('confirmPassword').isLength({ min: 1 }).withMessage('Confirm password is required'),
